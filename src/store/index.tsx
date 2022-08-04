6cf96e579b5bd316f810fba95dac9ec4e3121c04
youhavetosleep <qksekf4@gmail.com>
2022-08-04 21:47:07 +0900

    clean : 주석 정리

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,3 @@
-// const Test = () => {
-//   return <>전역 상태 관리 (recoil)관련 로직들이 모여있는 폴더입니다.</>;
-// };
-// export default Test
-
 import { atom } from "recoil";
 
 // recoil atom -> atom 은 state 의 일부를 나타냅니다. Atoms는 어떤 컴포넌트에서나 읽고 쓸 수 있고, 
